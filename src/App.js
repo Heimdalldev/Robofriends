022bd960f848c1e1e845aa8ae1de7a2eda7eead9
Daniel Lewis <daniel.lewis@heimdalldev.com>
2024-01-17 00:49:34 +0100

    added searchfunction

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
-import React from 'react';
-import CardList from './CardList'
+import React, { Component } from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { robots } from './robots';
 
-const App = () => {
-	return {
-		<CardList robots={ robots } />
-	};
+class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			robots: robots,
+			searchfield: ' '
+		}
+	}
+
+	onSearchChange = (event) => {
+		this.setState({ searchfield: event.target.value })
+	}
+
+	render() {
+		const filteredRobots = this.state.robots.filter(robots =>{
+			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		} )
+		return (
+			<div className="tc">
+				<h1>RoboFriends</h1>
+				<SearchBox searchChange={this.onSearchChange}/>
+				<CardList robots={ filteredRobots } />
+			</div>
+		);
+	};	
 }
+
+
+export default App;
+
+
+
+// Props are simply things that come out of STATE
+
+// STATE >> props
